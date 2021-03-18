e0e64c3ff866cddae50354382fa994081fa3140a
Krzysztof Dymianiuk <dvjcodec@gmail.com>
2021-03-18 18:42:42 +0100

    Update pins.h

--- src/include/pins.h
+++ src/include/pins.h
@@ -1,72 +1,53 @@
-// OUTPUT PINS
-// Solenoid output pins
-
-// Color placeholder
-// Orange / Orange-White = 12V key on (fused)
-// Brown / Brown-White = 5V key on
-// Blue = Speedo control voltage
-// Blue-White = NA
-// Green = Fuel pump control ground
-// Green-White -> blue-white = TPS signal
-
 #define TEENSY // we're running this on TEENSY
 
+// Outputs pins
+#define y3 4
+#define y4 3
+#define y5 4
+#define mpc 6
+#define spc 5
+#define tcc 9
+#define speedoCtrl 10 
+#define rpmMeter 26
+#define hornPin 31
+#define reversePin 22 //reverse light digital pin
 
-#define y3 36 // FMT3, orange<->brown/red // DOUT3, I need to use 16 as testing instead of 36.
-#define y4 35 // orange <-> brown/grey // DOUT2
-#define y5 8 //ex 14, orange <-> brown/black // DOUT1
-#define mpc 38 // red <-> brown/pink // DOUT5 // FMT3
-#define spc 37 // red <-> brown/yellow // DOUT4 // FMT3
-#define tcc 29 // pink <-> brown/yellow/white // DOUT6
-#define speedoCtrl 7 // blue <-> blue/green // DOUT7, 
-#define rpmMeter 30 // FMT2 missing // DOUT10
-#define boostCtrl 49 // FMT1, green <-> green/white/yellow // DOUT8, 3?
-#define fuelPumpCtrl 50 // missing // DOUT9, 13?
-#define hornPin 51 // Horn 
-
-#define reversePin 52 // reverse light digital pin
-
-// END OUTPUT PINS
-#define injectionPin 53 // should be 16.
-
-
-// INPUT PINS
-// Stick input
-#define whitepin 27 // 0.5kohm <-> yellow <-> grey-yellow-grey // DIN2 <-> blue
-#define bluepin 34// /0.5kohm <-> yellow <-> grey-green-grey // DIN4 <-> green
-#define greenpin 26 // 0.5kohm <-> yellow <-> grey-white-grey // DIN1 <-> whiteblue
-#define yellowpin 28//0.5kohm <-> yellow <-> grey-black-grey // DIN3 <-> whiteorange
+// Shifter input pins
+#define whitepin 29
+#define bluepin 27
+#define greenpin 30
+#define yellowpin 28
 
 // Switches
-#define autoSwitch 33 // ex. 22, 0.5kohm <-> yellow <-> grey-pink-grey // DIN5
+#define autoSwitch 12 // auto-manual switch W/S
+#define gdownSwitch 17 //gear down paddle manual
+#define gupSwitch 16 //gear up paddle manual
+#define lowGearPin  18 // TransferCase lowrange switch pin, +12V
 
-// KeyPad
-#define keypadPin  54 // Resistive keys pin, analog pin
+// Resistive keypad
+#define keypadPin  19 //54 // Resistive keys pin, analog pin
 
-//Low Gear Switch
-#define lowGearPin  55 // TransferCase lowrange switch pin, +12V
+// Car sensor input pins
+#define tpsPin A1
+#define atfPin A0
+#define oilPin A9
+#define n2pin 24
+#define n3pin 25
+#define speedPin 33
+#define rpmPin 32
+#define refPin A6
 
-
-#define gdownSwitch 23 // ex. 23 <-> NC // DIN6
-#define gupSwitch 24 // <-> NC // DIN7
-#define gupSwitchalt 31
-#define fuelInPin 32
-
-#define exhaustPresPin A12 // A12, A9 used in coupe.
-#define exhaustTempPin 12
-
-// Car sensor input pins, black
-#define tpsPin A2 // voltage div 5/3 <-> black <-> blue-black-blue = 1kohm/1.8kohm div // ANAIN3, boost? A3->A11
-#define atfPin A1 // voltage div 5/3 <-> black <-> pink = 1kohm/1.8kohm div // ANAIN2
-#define boostPin A11 // voltage div 5/3 <-> black <-> blue-brown-blue = 1kohm/1.8kohm div // ANAIN4, tps? A2->A10
-#define oilPin A0 // voltage div 12/3 <-> black <-> white-pink-white = 1kohm/380ohm div // ANAIN1
-#define n2pin 18 // voltage div 5/3 <-> black <-> whiteredwhite = 1kohm/1.8kohm div // DIN14
-#define n3pin 19 // voltage div 5/3 <-> black <-> brownredwhite = 1kohm/1.8kohm div // DIN15
-#define speedPin 20 // voltage div 12/3 <-> black <-> blueyellowblue = 1kohm/380ohm div // DIN10
-#define rpmPin 22 // voltage div 12/3 <-> black <-> whitebluewhite = 1kohm/380ohm div // DIN12
-#define batteryPin A10 // car battery monitor
-#define refPin A13 // sensor voltage ref ?
-//#define refPin A13 // using this in exhaustTempPin for now.
-
-#define exhaustTemperatureCS 9
-#define displayCS 10
+// Unused pins with Teensy 4.0
+     
+      #define batteryPin A6 //A7 // car battery monitor
+      //#define refPin A13 // using this in exhaustTempPin for now.
+      #define boostPin 46 //A11 // voltage div 5/3 <-> black <-> blue-brown-blue = 1kohm/1.8kohm div // ANAIN4, tps? A2->A10
+      #define exhaustPresPin 47 // A12, A9 used in coupe.
+      #define exhaustTemperatureCS 48
+      #define boostCtrl 49 // FMT1, green <-> green/white/yellow // DOUT8, 3?
+      #define fuelPumpCtrl 50 // missing // DOUT9, 13?
+      #define displayCS 51
+      #define fuelInPin 52
+      #define injectionPin 53 // should be 16.
+      #define exhaustTempPin 54
+      #define gupSwitchalt 55
